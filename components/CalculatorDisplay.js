ba4090fe371a65649474c4283471a8e6c32da4c8
bshoults <ben@bshoults.com>
2021-08-31 10:05:22 -0500

    Yo, hello world yo!

---
+++ components/CalculatorDisplay.js
@@ -0,0 +1,13 @@
+app.component('calculator-display', {
+  props: {
+    details: {
+      type: Array,
+      required: true
+    }
+  },
+  template:
+  /*html*/
+  `
+  <h1>Hello world!</h1>
+  `
+})
